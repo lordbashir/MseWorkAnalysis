582e971f5b26b3a3291a19c45673063213b9687a
Bashir <bashir@msegmbh.onmicrosoft.com>
2025-12-31 14:21:06 +0100

    Implemented and executed for 2025 analysis

---
+++ Models/MseProject.cs
@@ -0,0 +1,40 @@
+﻿namespace MseExcelAnalysis.Models
+{
+	public class MseProject
+	{
+		private readonly decimal _tagessatz1 = 1100.0m;
+		private readonly decimal _tagessatz2 = 1122.0m;
+		private readonly decimal _tagessatz3 = 600.0m;
+		private readonly decimal _tagessatz4 = 2000.0m;
+
+		private readonly List<string> _tagesSatz1Projects = new List<string>{"MBAG", "RAD-DB WARTUNG", "Rad-DB", "Rad-DB (AMG)", "QMSR" };
+		private readonly List<string> _tagesSatz3Projects = new List<string> { "SOLARSCHMIEDE" };
+		private readonly List<string> _tagesSatz4Projects = new List<string> { "Vesuv Wartung" };
+
+		public required string ProjectName { get; set; }
+		public decimal OrderAmount { get; set; }
+		public decimal AvailableWorkingDays
+		{
+			get
+			{
+				if(_tagesSatz1Projects.Contains(ProjectName))
+				{
+					return OrderAmount / _tagessatz1;
+				}
+				else if (_tagesSatz3Projects.Contains(ProjectName))
+				{
+					return OrderAmount / _tagessatz3;
+				}
+				else if (_tagesSatz4Projects.Contains(ProjectName))
+				{
+					return OrderAmount / _tagessatz4;
+				}
+				else
+				{
+					return OrderAmount / _tagessatz2;
+				}
+			}
+		}
+		public decimal CompletedWorkingDays { get; set; }
+	}
+}
--- Program.cs
+++ Program.cs
@@ -24,10 +24,41 @@ class Program
 				return;
 			}
 
+			var mseProjects = new List<MseProject>
+			{
+				new MseProject { ProjectName = "MBAG", OrderAmount = 120587.5m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "DTAG", OrderAmount = 274469.25m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "FUSO", OrderAmount = 120587.5m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "MSE", OrderAmount = 0.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "QMSR", OrderAmount = 25300.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "QMSR WARTUNG", OrderAmount = 14300.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "VESUV", OrderAmount = 124542.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "VESUV WARTUNG", OrderAmount = 24000.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "RAD-DB WARTUNG", OrderAmount = 18700.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "RAD-DB", OrderAmount = 75900.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "RAD-DB (AMG)", OrderAmount = 75900.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "REIFEN-DB", OrderAmount = 56100.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "REIFEN-DB WARTUNG", OrderAmount = 15400.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "REIFEN-DB AMG", OrderAmount = 20000.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "AV-DB", OrderAmount = 19074.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "AV-DB WARTUNG", OrderAmount = 19250.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "LV-DB", OrderAmount = 30294.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "LV-DB WARTUNG", OrderAmount = 19250.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "SOLARSCHMEIDE", OrderAmount = 34800.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "GESCHÄFTSFÜHRUNG", OrderAmount = 0.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "PROJEKT-MANAGEMENT", OrderAmount = 0.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "Fahrzeugverwaltung", OrderAmount = 0.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "Tagesgeschäft", OrderAmount = 0.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "Daily-Scrum-Meeting", OrderAmount = 0.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "KRANK", OrderAmount = 0.0m, CompletedWorkingDays = 0.0m },
+				new MseProject { ProjectName = "OTHERS", OrderAmount = 0.0m, CompletedWorkingDays = 0.0m }
+			};
+
 			var analysisFiles = Directory.GetFiles(args[0]);
 
 			List<SheetResult> combineSheetResult = new List<SheetResult>();
 
+			// Get analysis data for each timesheet excel file
 			foreach (var analysisFile in analysisFiles) 
 			{
 				FileInfo inputExcelFileInfo = new(analysisFile.Trim());
@@ -41,12 +72,92 @@ class Program
 				{
 					Log.Information("Analysis type is - Attendance");
 					var data = await ReadAttendanceExcelStreamingParallelAsync(inputExcelFileInfo);
+
+					// Combine the personal analysis result to prepare the report
 					foreach (var sheetResult in data)
 					{
 						combineSheetResult.Add(sheetResult);
 					}
 				}
-				
+			}
+
+			var allFlatRecords = combineSheetResult.SelectMany(res => res.Records.Where(r => !r.Projekt.ToLower().Equals("total")));
+
+			foreach (var flatRecord in allFlatRecords)
+			{
+				var projectFromTimesheet = flatRecord.Projekt.Trim().ToLowerInvariant();
+				var projectToSearch = "";
+
+				if(projectFromTimesheet == "trucks" || projectFromTimesheet == "emt dtag" || projectFromTimesheet == "trcuks")
+				{
+					projectToSearch = "DTAG";
+				}
+				else if (projectFromTimesheet == "cars")
+				{
+					projectToSearch = "MBAG";
+				}
+				else if (projectFromTimesheet == "fuso")
+				{
+					projectToSearch = "FUSO";
+				}
+				else
+				{
+					projectToSearch = projectFromTimesheet;
+				}
+
+				var projectSearchSubString = projectToSearch.Trim().ToLowerInvariant().Substring(0, 3);
+
+				var hasProjectEnding = projectFromTimesheet.Contains("amg")
+					|| projectFromTimesheet.Contains("(amg)")
+					|| projectFromTimesheet.Contains("@amg")
+					|| projectFromTimesheet.Contains("@amd")
+					|| projectFromTimesheet.Contains("wartung") 
+					|| projectFromTimesheet.Contains("amd");
+
+				if (hasProjectEnding)
+				{
+					var projectEndSearchSubString = projectToSearch.Trim().ToLowerInvariant().Substring(projectToSearch.Length-4, 4 );
+					if(projectEndSearchSubString.EndsWith("@amg")
+						|| projectEndSearchSubString.EndsWith("@amd")
+						|| projectEndSearchSubString.EndsWith("amg)"))
+					{
+						projectEndSearchSubString = "amg";
+					}
+
+					var project = mseProjects.Where(mp => mp.ProjectName.Trim().ToLowerInvariant().StartsWith(projectSearchSubString)
+					&& mp.ProjectName.Trim().ToLowerInvariant().EndsWith(projectEndSearchSubString))
+					.FirstOrDefault();
+					if (project != null) 
+					{ 
+						project.CompletedWorkingDays += flatRecord.Tagen;
+					}
+					else
+					{
+						var otherProject = mseProjects.Where(mp => mp.ProjectName.Trim().ToLowerInvariant().Equals("others")).FirstOrDefault();
+						if (otherProject != null)
+						{
+							otherProject.CompletedWorkingDays += flatRecord.Tagen;
+						}
+					}
+				}
+				else
+				{
+					var project = mseProjects.Where(mp => mp.ProjectName.Trim().ToLowerInvariant().StartsWith(projectSearchSubString)
+					&& (!mp.ProjectName.Trim().ToLowerInvariant().EndsWith("wartung"))).FirstOrDefault();
+					if (project != null)
+					{
+						project.CompletedWorkingDays += flatRecord.Tagen;
+					}
+					else
+					{
+						var otherProject = mseProjects.Where(mp => mp.ProjectName.Trim().ToLowerInvariant().Equals("others")).FirstOrDefault();
+						if (otherProject != null)
+						{
+							otherProject.CompletedWorkingDays += flatRecord.Tagen;
+						}
+					}
+				}
+
 			}
 
 			var teamName = args[0].Split('\\').Last();
@@ -55,6 +166,14 @@ class Program
 
 			await WritePdfAsync(combineSheetResult, resultPdfPath, teamName);
 			Log.Information("PDF successfully created");
+
+			// Create project summary for a team
+			var calculated = mseProjects.Where(mp => mp.CompletedWorkingDays > 0).ToList();
+			
+			string summaryPdfPath = System.IO.Path.Combine(args[0], teamName + "ProjectSummary.pdf");
+
+			await WriteSummaryPdfAsync(calculated, summaryPdfPath, teamName);
+			Log.Information("Summary PDF successfully created");
 
 		}
 		catch (Exception ex)
@@ -247,5 +366,45 @@ class Program
 			}
 		});
 	}
-	
+
+	static async Task WriteSummaryPdfAsync(List<MseProject> projects, string outputPath, string teamName)
+	{
+		await Task.Run(() =>
+		{
+			using var writer = new PdfWriter(outputPath);
+			using var pdf = new PdfDocument(writer);
+			using var document = new Document(pdf, PageSize.A4);
+
+			// Create fonts
+			PdfFont boldFont = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+			PdfFont regularFont = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+
+			//var title = fileNameWithoutExtension.ToUpper() + " - Working Reports";
+			var title = teamName.ToUpper() + " - Project Summary Report";
+
+			// Sheet header
+			document.Add(new Paragraph(title)
+				.SetFont(boldFont)
+				.SetFontSize(16));
+
+			// Table
+			Table table = new Table(4).UseAllAvailableWidth();
+			table.AddHeaderCell(new Cell().Add(new Paragraph("Projekt").SetFont(boldFont)));
+			table.AddHeaderCell(new Cell().Add(new Paragraph("OrderAmount").SetFont(boldFont)));
+			table.AddHeaderCell(new Cell().Add(new Paragraph("Available Work Days").SetFont(boldFont)));
+			table.AddHeaderCell(new Cell().Add(new Paragraph("Completed Work Days").SetFont(boldFont)));
+
+			foreach (var project in projects)
+			{
+				table.AddCell(new Cell().Add(new Paragraph(project.ProjectName).SetFont(regularFont)));
+				table.AddCell(new Cell().Add(new Paragraph(project.OrderAmount.ToString("F2", CultureInfo.InvariantCulture)).SetFont(regularFont)));
+				table.AddCell(new Cell().Add(new Paragraph(project.AvailableWorkingDays.ToString("F2", CultureInfo.InvariantCulture)).SetFont(regularFont)));
+				table.AddCell(new Cell().Add(new Paragraph(project.CompletedWorkingDays.ToString("F2", CultureInfo.InvariantCulture)).SetFont(regularFont)));
+			}
+
+			document.Add(table);
+			
+		});
+	}
+
 }
